1d93e7f1a750e1e1fe97cc576d17faaacfb26ff7
Amiya Behera <amiyatulu@gmail.com>
2019-12-28 12:58:15 +0530

    transfer

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,17 +1,39 @@
 pragma solidity >=0.4.22 <0.7.0;
 
 contract DappToken {
-    // Initialize the smart contract with number of tokens available. 
-    // To do this:
-    // Constructor
-    // Set the total number of tokens
-    // Read the total number of tokens
+    string public name = "DApp Token";
+    string public symbol = "DAPP";
+    string public standard = "DApp Token v1.0";
 
     uint256 public totalSupply;
 
-    constructor () public {
-        totalSupply = 1000000;
+    // Its there in documentation, please see it.
+
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+
+    );
+
+    mapping(address => uint256) public balanceOf;
+
+    constructor (uint256 _initialSupply) public {
+        balanceOf[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;
     }
 
+    // Transfer
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+    // Exception if account doesn't have enough
+    require(balanceOf[msg.sender] >= _value, "Don't have enough money");
+    // Transfer the balance
+    balanceOf[msg.sender] -= _value;
+    balanceOf[_to] += _value;
+    // Transfer Event
+    emit Transfer(msg.sender, _to, _value);
 
+    // Return a boolean
+    return true;
+    }
 }
